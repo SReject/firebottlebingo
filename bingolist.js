60b5bd5792dafea4e3a2a9c4a8847162109a5d89
SReject <sreject@comcast.net>
2016-05-24 04:04:09 -0400

    More Bingo Items

--- bingolist.js
+++ bingolist.js
@@ -5,31 +5,51 @@
         window.bingo = {};
     }
     window.bingo.items = [
+        // Quotes
         "\"I never...\"",
         "\"No death speed run\"",
         "\"I'm sure\"",
-        "Sarcastic 'yes'",
+        "\"So Spoopy\"",
+        "Sarcastic 'Yes'",
         "Sarcastic 'No'",
+        
+        // Promotions and Thanks
         "Promotes Another Streamer",
         "Promotes Beam",
         "Welcomes Viewer",
+        "Thanks Hoster",
         "Thanks New Follower",
         "Thanks For Donation",
         "Thanks Subcriber(s)",
+        
+        // Chat Interaction
         "Insults Chat",
         "Insults Specific Viewer",
         "New Bestie",
         "Cyber Hug",
         "Misses Obvious Joke",
+        "Evades Question",
+        "Makes Excuse",
+        
+        // Technical
         "Mentions using FTL",
         "Obs/Tachyon Fails to Capture Game",
         "Switches Ingest Server",
         "Game Crashes",
         "Soundboard Breaks",
         "Scottybot Messes Up",
+        
+        // Game Play
         "3 Deaths in Same Area",
         "3 Deaths by Same Enemy",
+        "Overlooks Obvious Route",
+        "Changes Game",
+        
+        // Meta
         "Eats on stream",
-        "Changes Game"
+        "Hydrates",
+        "Adjusts Mic",
+        "Wearing Hat",
+        "Not Wearing Hat"
     ];
 }());
