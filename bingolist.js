a93745b54db3d9ddc3b5e2e0f4b82217e38f9837
SReject <sreject@comcast.net>
2016-05-24 12:58:28 -0400

    More items

--- bingolist.js
+++ bingolist.js
@@ -6,51 +6,53 @@
     }
     window.bingo.items = [
         // Quotes
-        "\"I never...\"",
-        "\"No death speed run\"",
-        "\"I'm sure\"",
-        "\"So Spoopy\"",
-        "\"Don't quote that\"",
-        "Sarcastic 'Yes'",
-        "Sarcastic 'No'",
-        
+        '"I never..."',
+        '"No death speed run"',
+        '"I\'m sure"',
+        '"So Spoopy"',
+        '"Don\'t quote that"',
+        'Sarcastic "Yes"',
+        'Sarcastic "No"',
+
         // Promotions and Thanks
-        "Promotes Another Streamer",
-        "Promotes Beam",
-        "Welcomes Viewer",
-        "Thanks Hoster",
-        "Thanks New Follower",
-        "Thanks For Donation",
-        "Thanks Subcriber(s)",
-        
+        'Promotes Another Streamer',
+        'Promotes Beam',
+        'Welcomes Viewer',
+        'Thanks Hoster',
+        'Thanks New Follower',
+        'Thanks For Donation',
+        'Thanks Subcriber(s)',
+
         // Chat Interaction
-        "Insults Chat",
-        "Insults Specific Viewer",
-        "New Bestie",
-        "Cyber Hug",
-        "Misses Obvious Joke",
-        "Evades Question",
-        "Makes Excuse",
-        
+        'Insults Chat',
+        'Insults Specific Viewer',
+        'New Bestie',
+        'Cyber Hug',
+        'Misses Obvious Joke',
+        'Evades Question',
+        'Makes Excuse',
+        'Mentions Sparks',
+        'Mentions Pro-Account'
+
         // Technical
-        "Mentions using FTL",
-        "Obs/Tachyon Fails to Capture Game",
-        "Switches Ingest Server",
-        "Game Crashes",
-        "Restarts Soundboard",
-        "Scottybot Messes Up",
-        
+        'Mentions using FTL',
+        'Obs/Tachyon Fails to Capture Game',
+        'Switches Ingest Server',
+        'Game Crashes',
+        'Restarts Soundboard',
+        'Scottybot Messes Up',
+
         // Game Play
-        "3 Deaths in Same Area",
-        "3 Deaths by Same Enemy",
-        "Overlooks Obvious Route",
-        "Changes Game",
-        
+        '3 Deaths in Same Area',
+        '3 Deaths by Same Enemy',
+        'Overlooks Obvious Route',
+        'Changes Game',
+
         // Meta
-        "Eats on stream",
-        "Hydrates",
-        "Adjusts Mic",
-        "Wearing Hat",
-        "Not Wearing Hat"
+        'Eats on stream',
+        'Hydrates',
+        'Adjusts Mic',
+        'Wearing Hat',
+        'Not Wearing Hat'
     ];
 }());
